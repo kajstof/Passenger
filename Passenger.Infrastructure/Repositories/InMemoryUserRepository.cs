59497af501d273448e4ecf8c52b7fb60d167ebe0
Krzysztof Krysiak <krysiak86@gmail.com>
2019-01-13 16:34:04 +0100

    Implement methods in InMemoryUserRepository

--- Passenger.Infrastructure/Repositories/InMemoryUserRepository.cs
+++ Passenger.Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Passenger.Core.Domain;
 using Passenger.Core.Repository;
 
@@ -7,34 +8,32 @@ namespace Passenger.Infrastructure.Repositories
 {
     public class InMemoryUserRepository : IUserRepository
     {
+        private static ISet<User> _users = new HashSet<User>();
+
         public User Get(Guid id)
         {
-            throw new NotImplementedException();
+            return _users.Single(x => x.Id == id);
         }
 
         public User Get(string email)
         {
-            throw new NotImplementedException();
+            return _users.Single(x => x.Email.Equals(email.ToLowerInvariant()));
         }
 
-        public IEnumerable<User> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
+        public IEnumerable<User> GetAll() => _users;
+        
         public void Add(User user)
         {
-            throw new NotImplementedException();
+            _users.Add(user);
         }
 
         public void Remove(Guid id)
         {
-            throw new NotImplementedException();
+            _users.Remove(Get(id));
         }
 
         public void Update(User user)
         {
-            throw new NotImplementedException();
         }
     }
 }
