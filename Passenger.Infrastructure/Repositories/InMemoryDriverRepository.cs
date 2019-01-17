dade01d5bb0d11bb56e091026c38f4d4990759db
Krzysztof Krysiak <krysiak86@gmail.com>
2019-01-17 21:06:21 +0100

    Add constructors for models, repository, service and dto for driver

---
+++ Passenger.Infrastructure/Repositories/InMemoryDriverRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Passenger.Core.Domain;
+using Passenger.Core.Repository;
+
+namespace Passenger.Infrastructure.Repositories
+{
+    class InMemoryDriverRepository : IDriverRepository
+    {
+        private static ISet<Driver> _drivers = new HashSet<Driver>();
+
+        public void Add(Driver driver)
+        {
+            _drivers.Add(driver);
+        }
+
+        public Driver Get(Guid userId) => _drivers.Single(x => x.UserId == userId);
+
+        public IEnumerable<Driver> GetAll() => _drivers;
+
+        public void Update(Driver driver)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Passenger.Infrastructure/Services/DriverService.cs
@@ -0,0 +1,26 @@
+using System;
+using Passenger.Core.Repository;
+using Passenger.Infrastructure.DTO;
+
+namespace Passenger.Infrastructure.Services
+{
+    public class DriverService : IDriverService
+    {
+        private readonly IDriverRepository _driverRepository;
+
+        public DriverService(IDriverRepository driverRepository)
+        {
+            _driverRepository = driverRepository;
+        }
+
+        public DriverDto Get(Guid userId)
+        {
+            var driver = _driverRepository.Get(userId);
+
+            return new DriverDto
+            {
+                //id 
+            };
+        }
+    }
+}
